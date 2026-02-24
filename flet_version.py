9de03a09e1429d65aeaf2020b91aceba02734875
Mell <julietmell44@gmail.com>
2026-02-24 08:40:38 -0400

    Added word list

--- flet_version.py
+++ flet_version.py
@@ -1,2 +1,15 @@
 import flet as ft
 import random
+
+word_list = [
+    "princess",
+    "glitter",
+    "sparkle",
+    "bestie",
+    "pink",
+    "slay",
+    "iconic",
+    "queen",
+    "fabulous",
+    "dreamy"
+]
