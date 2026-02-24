3469905973eabbbf63d519b1b7dbffb8d3ee61fb
Mell <julietmell44@gmail.com>
2026-02-24 08:51:32 -0400

    fixed check button structure y moree

--- flet_version.py
+++ flet_version.py
@@ -24,10 +24,24 @@ def main(page: ft.Page):
     user_input = ft.TextField(label="Type the word here ✨")
     result_text = ft.Text("")
 
+    def check_word(e):
+        if user_input.value == word_display.value:
+            result_text.value = "Correct (remember mell is the best) 💖"
+        else:
+            result_text.value = "Try again loseeerrr 😜"
+
+        random.shuffle(word_list)
+        word_display.value = word_list[0]
+        user_input.value = ""
+        page.update()
+
+    check_button = ft.ElevatedButton("Check ✨", on_click=check_word)
+
     page.add(
         word_display,
         user_input,
-        result_text
+        result_text,
+        check_button
     )
 
 ft.app(target=main)
